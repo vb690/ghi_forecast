77499c2878e48c160c3502faba2395058b9c39c2
vb690 <bonomettivalerio@gmail.com>
2023-10-29 22:42:32 +0000

    Final commit

--- modules/models/torch_backend/data_handlers.py
+++ modules/models/torch_backend/data_handlers.py
@@ -1,7 +1,9 @@
 from torch.utils.data import Dataset
 
-
 class TimeseriesAutoregressiveDataset(Dataset):
+    """Dataset providing data for an autoregressive model
+    in a streaming fashion. This is required for very large datasets.
+    """
     def __init__(self, X, y, ar_window, forecast_horizon, gap):
         self._X = X
         self._y = y
@@ -10,13 +12,11 @@ class TimeseriesAutoregressiveDataset(Dataset):
         self._forecast_horizon = forecast_horizon
 
     def __len__(self):
-        seq_len = (
-            self._X.__len__() - (self._forecast_horizon + self._ar_window)
-        ) / self._gap
+        seq_len = (self._X.__len__() - (self._forecast_horizon + self._ar_window)) / self._gap
         return int(seq_len)
 
     def __getitem__(self, index):
         index = index * self._gap
-        end_insample = index + self._ar_window
+        end_insample = index+self._ar_window
         end_outsample = end_insample + self._forecast_horizon
-        return (self._X[index:end_insample], self._y[end_insample:end_outsample])
+        return (self._X[index: end_insample], self._y[end_insample: end_outsample])
